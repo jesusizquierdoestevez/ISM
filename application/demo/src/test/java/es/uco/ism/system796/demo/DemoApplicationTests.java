479bf8fd88604c255496bd356e3b67d32a210988
Rafael Egea Jurado <55484111+i82egjur@users.noreply.github.com>
2021-12-29 19:32:09 +0100

    Add files via upload

---
+++ application/demo/src/test/java/es/uco/ism/system796/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package es.uco.ism.system796.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootApplication
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
